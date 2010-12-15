86f8d70b25c317994d71ce00735bda026ef2b93a
Marek Kowalcze <marek.kowalcze@gmail.com>
2010-12-15 23:03:40 +0100

    rewrite to hmal

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 
-get '/' do
-  redirect '/index.html'
+MENU = [
+  { :page => :main,     :title => "Strona główna",            :label => "Strona główna" },
+  { :page => :apel,     :title => "Treść naszego Apelu",      :label => "Coś o mnie" },
+  { :page => :historia, :title => "Poznaj historię choroby",  :label => "Choroba" },
+  { :page => :dok,      :title => "Dokumenty",                :label => "Dokumenty" },
+  { :page => :klinika,  :title => "Klinika NeuroVita",        :label => "O Klinice"},
+  { :page => :cel,      :title => "Na co zbieramy?",          :label => "Jak pomóc ?"},
+  { :page => :zdjecia,  :title => "Kilka zdjęć...",           :label => "Zdjęcia"}
+]
+
+get('/') do
+  @menu = MENU
+  haml :index
+end
+
+MENU.map{|e| e[:page] }.each do |page|
+  get("/#{page.to_s}") do
+    @menu = MENU
+    haml page.to_sym
+  end
 end
