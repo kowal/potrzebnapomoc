ddec62da33dde11443589f1edc42f3926454132a
Marek Kowalcze <marek.kowalcze@gmail.com>
2014-01-03 22:50:50 +0100

    Ruby 2.0, comments

--- app.rb
+++ app.rb
@@ -1,17 +1,19 @@
 require 'sinatra'
 
 MENU = [
-  { :page => :main,     :title => "Strona główna",            :label => "Strona główna" },
-  { :page => :apel,     :title => "Treść naszego Apelu",      :label => "Coś o mnie" },
-  { :page => :historia, :title => "Poznaj historię choroby",  :label => "Choroba" },
-  { :page => :dok,      :title => "Dokumenty",                :label => "Dokumenty" },
-  { :page => :klinika,  :title => "Klinika NeuroVita",        :label => "O Klinice"},
-  { :page => :cel,      :title => "Na co zbieramy?",          :label => "Jak pomóc?"},
-  { :page => :zdjecia,  :title => "Kilka zdjęć...",           :label => "Zdjęcia"}
+  { page: :main,     title: 'Strona główna',           label: 'Strona główna' },
+  { page: :apel,     title: 'Treść naszego Apelu',     label: 'Coś o mnie' },
+  { page: :historia, title: 'Poznaj historię choroby', label: 'Choroba' },
+  { page: :dok,      title: 'Dokumenty',               label: 'Dokumenty' },
+  { page: :klinika,  title: 'Klinika NeuroVita',       label: 'O Klinice'},
+  { page: :cel,      title: 'Na co zbieramy?',         label: 'Jak pomóc?'},
+  { page: :zdjecia,  title: 'Kilka zdjęć...',          label: 'Zdjęcia'}
 ]
 
 settings.root = File.expand_path '.'
 
+# To prevent sending the X-Frame-Options
+# (this is needed when application is displayed in frame from other domain)
 set :protection, :except => :frame_options
 
 get('/') do
@@ -20,8 +22,8 @@ get('/') do
 end
 
 MENU.map{|e| e[:page] }.each do |page|
-  get("/#{page.to_s}") do
+  get("/#{page}") do
     @menu = MENU
-    haml page.to_sym
+    haml page
   end
 end
